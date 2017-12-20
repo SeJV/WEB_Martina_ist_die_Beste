e66b7d522c1d952f56e7d0c35870deb5379eb263
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-12-20 13:47:33 +0100

    umschalt alt f

--- battleship-web/server/server.js
+++ battleship-web/server/server.js
@@ -14,101 +14,121 @@ let player2Socket;
 let player1Field;
 let player2Field;
 
-let player1FieldOfShots = shiplogic.createField();
-let player2FieldOfShots = shiplogic.createField();
+let player1FieldOfSafedFire = shiplogic.createField();
+let player2FieldOfSafedFire = shiplogic.createField();
 
 const PLAYER_1_TURN = 1;
 const PLAYER_2_TURN = 2;
+
+const SHOT_IN_WATER = 1;
+const ShOT_ON_SHIP = 2;
 let turn = PLAYER_1_TURN;
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "battleship.html");
+router.get("/", function (req, res) {
+    res.sendFile(path + "battleship.html");
 });
 
 app.use(servestatic('public'));
 
-app.use("/",router);
+app.use("/", router);
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    if(!player1Socket){
+    if (!player1Socket) {
         player1Socket = socket;
-        player1Socket.on('disconnect', function(){
+        player1Socket.on('disconnect', function () {
             console.log("Player1 disconnected");
             player1Socket = undefined;
         })
-        player1Socket.on('fire', function(x,y){
-            if(player1Socket && player2Socket && turn===PLAYER_1_TURN){
-                if(player2Field[y][x]){
+        player1Socket.on('fire', function (x, y) {
+            if (player1Socket && player2Socket && turn === PLAYER_1_TURN) {
+                if (player2Field[y][x]) {
                     player1Socket.emit('fireResult', true);
-                    player2Socket.emit('fireResultEnemy',x ,y ,true);
+                    player2Socket.emit('fireResultEnemy', x, y, true);
                     turn = PLAYER_1_TURN;
                     emitTurn();
+                    saveFireOfPlayer1(x, y, true);
                 }
-                else{
+                else {
                     player1Socket.emit('fireResult', false);
-                    player2Socket.emit('fireResultEnemy',x ,y ,false);
+                    player2Socket.emit('fireResultEnemy', x, y, false);
                     turn = PLAYER_2_TURN;
                     emitTurn();
+                    saveFireOfPlayer1(x, y, false);
                 }
             }
         })
     }
-    else if(!player2Socket){
+    else if (!player2Socket) {
         player2Socket = socket;
-        if(!player1Field && !player2Field){
+        if (!player1Field && !player2Field) {
             player1Field = shipplacement.generateShipPlacement();
             player2Field = shipplacement.generateShipPlacement();
         }
         player1Socket.emit('myShips', player1Field);
         player2Socket.emit('myShips', player2Field);
-        
-        player1Socket.emit('playerNumber', 1);
-        player2Socket.emit('playerNumber', 2);
-        
+
         player1Socket.emit('playerTurn', turn);
         player2Socket.emit('playerTurn', turn);
 
-        player2Socket.on('disconnect', function(){
+        player2Socket.on('disconnect', function () {
             console.log("Player2 disconnected");
             player2Socket = undefined;
         })
-        player2Socket.on('fire', function(x,y){
-            if(player1Socket && player2Socket && turn ===PLAYER_2_TURN){
-                if(player1Field[y][x]){
-                    player1Socket.emit('fireResultEnemy',x ,y ,true);
+        player2Socket.on('fire', function (x, y) {
+            if (player1Socket && player2Socket && turn === PLAYER_2_TURN) {
+                if (player1Field[y][x]) {
+                    player1Socket.emit('fireResultEnemy', x, y, true);
                     player2Socket.emit('fireResult', true);
                     turn = PLAYER_2_TURN;
                     emitTurn();
+                    saveFireOfPlayer2(x, y, true);
                 }
-                else{
-                    player1Socket.emit('fireResultEnemy',x ,y ,false);
+                else {
+                    player1Socket.emit('fireResultEnemy', x, y, false);
                     player2Socket.emit('fireResult', false);
                     turn = PLAYER_1_TURN;
                     emitTurn();
+                    saveFireOfPlayer2(x, y, false);
                 }
             }
         })
     }
-  });
+});
 
-http.listen(3000,function(){
-  console.log("Live at Port 3000");
-});  
+http.listen(3000, function () {
+    console.log("Live at Port 3000");
+});
 
-function emitTurn(){
-    player1Socket.emit('playerTurn', turn);
-    player2Socket.emit('playerTurn', turn);
+function emitTurn() {
+    if (turn == PLAYER_1_TURN) {
+        player1Socket.emit('playerTurn', true);
+        player2Socket.emit('playerTurn', false);
+    }
+    else {
+        player1Socket.emit('playerTurn', false);
+        player2Socket.emit('playerTurn', true);
+    }
 }
 
-function player1ShotFired(x,y){
-    player1FieldOfShots[y][x] = 1;
+function saveFireOfPlayer1(x, y, wasHit) {
+    if (wasHit) {
+        player1FieldOfSafedFire[y][x] = 2;
+    }
+    else {
+        player1FieldOfSafedFire[y][x] = 1;
+    }
 }
-function player2ShotFired(x,y){
-    player2FieldOfShots[y][x] = 1;
+function saveFireOfPlayer2(x, y, wasHit) {
+    if (wasHit) {
+        player2FieldOfSafedFire[y][x] = 2;
+    }
+    else {
+        player2FieldOfSafedFire[y][x] = 1;
+    }
 }
--- battleship-web/server/ship-logic.js
+++ battleship-web/server/ship-logic.js
@@ -11,7 +11,15 @@ function createField() {
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 }
 
+function destroyedShips(playerField, opponentsShots){
+  destroyedShips = createField();
+  for(y = 0; y < 10; y++){
+    for(x = 0; x < 10; x++){
 
+    }
+  }
+
+}
 
 module.exports = {
   createField: createField,
