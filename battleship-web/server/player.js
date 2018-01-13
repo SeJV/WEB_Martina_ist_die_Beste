2c1f7a13532037a80ebd7a91de40b68309ceae6a
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2018-01-13 10:33:29 +0100

    alles passt

--- battleship-web/server/player.js
+++ battleship-web/server/player.js
@@ -4,8 +4,7 @@ const shipPlacement = require(path.join(__dirname, 'ship_placement'));
 const shipLogic = require(path.join(__dirname, 'ship_logic'));
 const Highscore = require(path.join(__dirname, 'highscore'));
 const Score = require(path.join(__dirname, 'score'));
-//const highscorePath = path.join(__dirname, 'highscore.json');
-const highscorePath = 'C://highscore.json';
+const highscorePath = path.join(__dirname, 'highscore.json');
 const Coordinate = require(path.join(__dirname, 'coordinate'));
 
 module.exports = class Player {
@@ -181,14 +180,14 @@ module.exports = class Player {
 
         let highscore = new Highscore();
         if(!highscore.readHighscore(highscorePath)) {
-            let msg = "Error reading from " + highscorePath;
+            let msg = 'Error reading from ' + highscorePath;
             console.log(msg);
             this._playerSocket.emit('highscore_error', msg);
             this._opponentPlayer.playerSocket.emit('highscore_error', msg);
         }
         highscore.addScore(new Score(this.name, this._score));
         if(!highscore.writeHighscore(highscorePath)) {
-            let msg = "Error writing in " + highscorePath;
+            let msg = 'Error writing in ' + highscorePath;
             console.log(msg);
             this._playerSocket.emit('highscore_error', msg);
             this._opponentPlayer.playerSocket.emit('highscore_error', msg);
